74b67b4cfc6ce9390c17c6fea9dc9ec112942b82
ikona23 <ikona23@gmail.com>
2016-11-05 19:28:56 +1300

    roman numerals

---
+++ index-min.js
@@ -0,0 +1,1 @@
+switch(prompt("give me a number")){case"1":document.write("I");break;case"2":document.write("II");break;case"3":document.write("III");break;case"4":document.write("IV");break;case"5":document.write("IV");break;case"6":document.write("VI");break;case"8":document.write("VIII");break;case"9":document.write("IV");break;case"10":document.write("X")}
--- index.js
+++ index.js
@@ -1,5 +1,38 @@
-// reverse a string
-function reverseString(str) {
-  var strReverse = str.split('').reverse().join('');
-  return strReverse;
+// function rev(word) {
+//   var newWord = word.split('')
+//                     .reverse()
+//                     .join('')
+//   return newWord
+// }
+// document.write(rev('nani'))
+
+
+switch (prompt("give me a number")) {
+  case '1':
+    document.write('I')
+    break;
+  case '2':
+    document.write('II')
+    break;
+  case '3':
+    document.write('III')
+    break;
+  case '4':
+    document.write('IV')
+    break;
+  case '5':
+    document.write('IV')
+  break;
+  case '6':
+    document.write('VI')
+  break;
+  case '8':
+    document.write('VIII')
+  break;
+  case '9':
+    document.write('IV')
+  break;
+  case '10':
+    document.write('X')
+  break;
 }
