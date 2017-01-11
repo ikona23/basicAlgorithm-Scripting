05ee3f2d970ae42e6787fb478e0a3ba354b7f478
ikona23 <ikona23@gmail.com>
2017-01-11 18:03:34 +1300

    getting data from spotify

--- promise.js
+++ promise.js
@@ -1,9 +1,9 @@
-function sync() {
-  setTimeout(deferred,1000)
-  console.log('sync');
-}
-function deferred() {
-  console.log('deferred');
-}
- sync()
- deferred()
+fetch('https://api.spotify.com/v1/search?query=psytrance&offset=0&limit=20&type=artist')
+.then(response =>response.json())
+.then(data=> {
+  const uris = data.artists.items.map(item=> {
+    return item.uri
+  }).slice(0,5)
+  console.log(uris)
+  document.write(uris)
+})
